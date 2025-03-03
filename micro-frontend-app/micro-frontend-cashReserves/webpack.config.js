17019aec4cf8ad278af1a6d5d9b7d8b6fcae30a4
Jyothi <chinda.jyothi@gmail.com>
2025-03-03 05:56:07 -0600

    added code for cashreserves

---
+++ micro-frontend-app/micro-frontend-cashReserves/webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 3006,
+  },
+  entry: "./src/index.js",
+  output: {
+    publicPath: "auto",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"], 
+  },
+  
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "cashReserves",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./CashReservesPage": "./src/bootstrap",
+      },
+      shared: {
+        react: { singleton: true, requiredVersion: "^19.0.0" },
+        "react-dom": { singleton: true, requiredVersion: "^19.0.0" },
+        swr: { singleton: true, requiredVersion: "^2.3.2" },
+        antd: { singleton: true, requiredVersion: "^5.24.1" },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options:{
+            presets: ["@babel/preset-react","@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"]
+          }
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
