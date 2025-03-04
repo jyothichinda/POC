5748c54c4ab8f60046d897aac008281671bbb732
Jyothirmayi Chinda <jyothirmayi.chinda@dataknol.com>
2025-03-04 12:55:41 +0530

    Select and Deselect

--- micro-frontend-app/micro-frontend-pooling/src/App.jsx
+++ micro-frontend-app/micro-frontend-pooling/src/App.jsx
@@ -1,8 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PoolingTable from "./components/PoolingTable";
 
 const App = () => {
-  return <PoolingTable />;
+  const [data, setData] = useState([
+    {
+      name: "ABC",
+      master: "ABC Bank",
+      currency: "USD",
+      participating: "ABC-1, ABC-2",
+      status: "Active",
+      execute: "02/27/2025",
+      balance: "$500,000",
+      liquidity: "$100,000",
+      update: "02/25/2025",
+      interest: "1.5%",
+      rebalancing: "Yes",
+      action: "View / Edit",
+    },
+    {
+      name: "DEF",
+      master: "DEF Bank",
+      currency: "INR",
+      participating: "",
+      status: "InActive",
+      execute: "",
+      balance: "",
+      liquidity: "",
+      update: "",
+      interest: "",
+      rebalancing: "",
+      action: "",
+    },
+  ]);
+
+  useEffect(() => {
+    const eventSource = new EventSource("backend url");
+
+    eventSource.onmessage = (event) => {
+      try {
+        const newData = JSON.parse(event.data);
+        setData((prev) => [...prev, newData]);
+      } catch (error) {
+        console.error("Error parsing SSE data:", error);
+      }
+    };
+
+    eventSource.onerror = () => {
+      console.error("SSE Connection Failed!");
+      eventSource.close();
+    };
+
+    return () => eventSource.close();
+  }, []);
+
+  return <PoolingTable data={data} />;
 };
 
 export default App;
