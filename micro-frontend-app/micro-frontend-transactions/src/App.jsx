5f5df6beff61417241c47778cdfa9ae021782abe
Jyothi <chinda.jyothi@gmail.com>
2025-03-03 23:45:45 -0600

    added SWR api call code on transactions page

--- micro-frontend-app/micro-frontend-transactions/src/App.jsx
+++ micro-frontend-app/micro-frontend-transactions/src/App.jsx
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TransactionsTable from "./components/TransactionsTable";
 
 const App = () => {
-  return <TransactionsTable />;
+  const [data, setData] = useState([
+    {
+      id: "MSG123",
+      creditor: "XYZ LTD",
+      debtor: "ABC Corp",
+      network: "$500",
+      tier: 1,
+      status: "Completed",
+      amount: "$12345.678",
+      cashflow: "Cash Outflow",
+      time: "03/03/2025 11:35:27",
+    },
+    {
+      id: "MSG124",
+      debtor: "National Bank",
+      creditor: "ABC Corp",
+      network: "$5800",
+      tier: 1,
+      status: "Pending",
+      amount: "$38945.12",
+      cashflow: "Cash Inflow",
+      time: "03/03/2025 16:45:19",
+    },
+  ]);
+
+  useEffect(() => {
+    const eventSource = new EventSource("backend url");
+
+    eventSource.onmessage = (event) => {
+      try {
+        const newData = JSON.parse(event.data);
+        setData((prev) => [...prev, newData]);
+      } catch (error) {
+        console.error("Error parsing SSE data:", error);
+      }
+    };
+
+    eventSource.onerror = () => {
+      console.error("SSE Connection Failed!");
+      eventSource.close();
+    };
+
+    return () => eventSource.close();
+  }, []);
+
+  return <TransactionsTable data={data} />;
 };
 
 export default App;
--- micro-frontend-app/micro-frontend-transactions/src/components/TransactionsTable.jsx
+++ micro-frontend-app/micro-frontend-transactions/src/components/TransactionsTable.jsx
@@ -33,31 +33,6 @@ const allColumns = [
   { title: "TimeStamp", dataIndex: "time", key: "time" },
 ];
 
-const data = [
-  {
-    id: "MSG123",
-    creditor: "XYZ LTD",
-    debtor: "ABC Corp",
-    network: "$500",
-    tier: 1,
-    status: "Completed",
-    amount: "$12345.678",
-    cashflow: "Cash Outflow",
-    time: "03/03/2025 11:35:27",
-  },
-  {
-    id: "MSG124",
-    debtor: "National Bank",
-    creditor: "ABC Corp",
-    network: "$5800",
-    tier: 1,
-    status: "Pending",
-    amount: "$38945.12",
-    cashflow: "Cash Inflow",
-    time: "03/03/2025 16:45:19",
-  },
-];
-
 // Sortable item component
 const SortableItem = ({ column, isChecked, onToggle }) => {
   const { attributes, listeners, setNodeRef, transform, transition } =
@@ -97,7 +72,7 @@ const SortableItem = ({ column, isChecked, onToggle }) => {
   );
 };
 
-const TransactionsTable = () => {
+const TransactionsTable = ({ data }) => {
   // Load preferences from local storage
   const savedColumns =
     JSON.parse(localStorage.getItem("selectedColumns")) ||
